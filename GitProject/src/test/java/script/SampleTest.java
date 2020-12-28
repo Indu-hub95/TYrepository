9a7984f5ad477ce183cf3eb4441c013068c2a31a
admin <admin@DESKTOP-JU253R0>
2020-12-28 21:37:25 +0530

    push

---
+++ GitProject/src/test/java/script/SampleTest.java
@@ -0,0 +1,14 @@
+package script;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	
+	   public void disp(){
+		System.out.println("Hello");
+		System.out.println("world");
+	
+	}
+}
